8e71e5afdded3b9cef326c5d92250ad48bb3aff0
luis-8 <luis_zavala8@outlook.com>
2020-11-05 22:42:05 -0600

    Se creó y añadió a la actividad principal el widget ToolBar

---
+++ app/src/main/java/com/luis/toolbar/MainActivity.java
@@ -0,0 +1,19 @@
+package com.luis.toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.tool_bar_main);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Mi título");
+    }
+}
